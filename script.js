9922d1d75130385c9423b78e99d2b10d11f0f4c3
csnova <chandlersnova@gmail.com>
2023-08-03 15:12:38 -0700

    Create Grid Create grid of 16 by 16 that the etch a sketch will be in

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container");
+for (let i = 1; i <= 16; i++) {
+  const row = document.createElement("div");
+  row.classList.add(`row${i}`);
+  row.classList.add(`row`);
+  row.classList.add("container");
+  container.appendChild(row);
+  for (let j = 1; j <= 16; j++) {
+    document.querySelector(".row");
+    const block = document.createElement("div");
+    block.classList.add(`block${i}-${j}`);
+    block.classList.add("block");
+    block.classList.add(`row${i}`);
+    block.classList.add(`column${j}`);
+    row.appendChild(block);
+  }
+}
